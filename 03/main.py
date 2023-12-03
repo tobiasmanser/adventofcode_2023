160a55c993a79bc49ea5d5809744582609c9ae22
Tobias Manser <tobias_manser@hotmail.ch>
2023-12-03 12:48:10 +0100

    - Solved Day 3 Part 2

--- 03/main.py
+++ 03/main.py
@@ -12,7 +12,7 @@ import re
 def main():
     cwd = Path(__file__).parent.resolve()
     input = get_input(cwd)
-    print(valid_numbers(input))
+    print(engine_ratio(input))
 
 def get_input(cwd):
     with open(cwd / 'input.txt', 'r') as f:
@@ -45,5 +45,30 @@ def valid_numbers(lines):
                         sum += int(m.group())
                         continue
     return sum
+
+def engine_ratio(lines):
+    ratio = 0
+    for i in range(0, len(lines)):
+        matches = re.finditer(r'\*', lines[i])
+        for m in matches:
+            found = []
+            found.extend(find_engines(lines[i], m.start()))
+            if i + 1 < len(lines):
+                found.extend(find_engines(lines[i+1], m.start()))
+            if i > 0:
+                found.extend(find_engines(lines[i-1], m.start()))
+            print(found)
+            if len(found) == 2:
+                ratio += found[0] * found[1]
+    return ratio
+            
+
+def find_engines(line, star_index):
+    engines = []
+    numbers = re.finditer(r'\d+', line)
+    for n in numbers:
+        if n.start() <= star_index + 1 and n.end() > star_index - 1:
+            engines.append(int(n.group()))
+    return engines
 
 main()
